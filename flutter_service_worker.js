3a67a256124f60e82aa1fac0e523a7d058a008bb
sjtsh <74091619+sjtsh@users.noreply.github.com>
2021-06-14 15:11:28 +0545

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,8 +39,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "7d9e0480737a62795b9fceec125ae95f",
-"/": "7d9e0480737a62795b9fceec125ae95f",
+"index.html": "206e50dad490ad398f2b4b4e889acd5b",
+"/": "206e50dad490ad398f2b4b4e889acd5b",
 "main.dart.js": "247c567a52029324430523869933db56",
 "manifest.json": "5a2c0526f1fe73e6f211bc7a9d15c210",
 "version.json": "426313f2f3133c2f20415344c4a22df3"
